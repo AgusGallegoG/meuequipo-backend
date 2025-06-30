0e547c4bb267fff1282514a9d947ebe5db60f263
AgusGallegoG <agus.verin7@gmail.com>
2025-07-01 00:08:28 +0200

    Added cache-control 1 hour images serving
    Signed-off-by: AgusGallegoG <agus.verin7@gmail.com>

--- src/main/java/com/web/meuequipo/core/image/rest/ImageController.java
+++ src/main/java/com/web/meuequipo/core/image/rest/ImageController.java
@@ -4,6 +4,7 @@ import com.web.meuequipo.core.image.dto.ImageViewDTO;
 import com.web.meuequipo.core.image.exception.ImageException;
 import com.web.meuequipo.core.image.service.ImageService;
 import org.springframework.core.io.Resource;
+import org.springframework.http.CacheControl;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/api/images")
@@ -34,7 +36,9 @@ public class ImageController {
             Resource resource = imageService.serveImage(id);
             String contentType = Files.probeContentType(resource.getFile().toPath());
 
-            return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType != null ? contentType : "application/octet-stream")).body(resource);
+            return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType != null ? contentType : "application/octet-stream"))
+                    .cacheControl(CacheControl.maxAge(1, TimeUnit.HOURS))
+                    .body(resource);
         } catch (ImageException e) {
             return ResponseEntity.notFound().build();
         } catch (IOException e) {
